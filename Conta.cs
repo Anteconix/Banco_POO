06b43f5c2eac9184122629733df1380e308694dc
Anteconix <magnofilho2323@gmail.com>
2023-10-17 19:27:23 -0300

    Adicionar arquivos de projeto.

---
+++ Conta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Conta
+    {
+        // propriedades
+        public string numero;
+        public string titular;
+        public decimal saldo;
+        public List<Transacao> transacoes = new List<Transacao>();
+
+        // método construtor
+        public Conta(string num, string tit) 
+        { 
+            this.numero = num;
+            this.titular = tit;
+            this.saldo = 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using Banco;
+
+// "prepara" o nosso banco de dados
+List<Conta> bancoDados = new List<Conta>();
+
+// prepara a tela do computador para ser usada pelo software
+Tela tela = new Tela();
+tela.configurarTela();
+
+// crias as variáveis que serão usadas no menu de opções 
+string opcao;
+List<string> menuPrincipal = new List<string>();
+menuPrincipal.Add("1 - Lançar transação ");
+menuPrincipal.Add("2 - Emitir extrato   ");
+menuPrincipal.Add("3 - Consultar conta  ");
+menuPrincipal.Add("4 - Bloqueio de conta");
+menuPrincipal.Add("5 - Cadastro de conta");
+menuPrincipal.Add("0 - Sair             ");
+
+// agora vem o programa de verdade
+while (true)
+{
+    tela.montarTelaSistema("Banco de Sistema");
+    opcao = tela.mostrarMenu(menuPrincipal, 3, 3);
+
+    // o usuario pediu pra sair do sistema
+    if (opcao == "0") break;
+
+    // usuario pediu pra cadastrar uma nova cona
+    if (opcao == "5")
+    {
+        tela.montarMoldura(7, 7, 40, 12, "Nova conta");
+
+        // pergunta o numero da conta
+        Console.SetCursorPosition(8, 9);
+        Console.Write("Numero  : ");
+        string num = Console.ReadLine();
+
+        // pergunta o titular da conta
+        Console.SetCursorPosition(8, 10);
+        Console.Write("Titular :");
+        string tit = Console.ReadLine();
+
+        // solicita confirmação para cadastro
+        Console.SetCursorPosition(8, 11);
+        Console.Write("Confirma cadastro (S/N) : ");
+        string resp = Console.ReadLine();
+
+        // coloca a nova conta no BD caso o usuário confirme
+        // o cadastro
+        if (resp.ToUpper()=="S")
+        {
+            // "cadastra" no BD
+            bancoDados.Add(  new Conta(num,tit)  );
+
+            // Conta c = new Conta(num,tit);
+            // bancoDados.Add(c);
+        }
+    }
+}
